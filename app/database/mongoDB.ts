f7aea42a58983ddf342bb06715e6e06b7621bcec
ayshiff <remi.d45@gmail.com>
2018-10-22 14:27:16 +0200

    Add ts-node dependencies

--- app/database/mongoDB.ts
+++ app/database/mongoDB.ts
@@ -1,6 +1,8 @@
 import config from '../config/mongo.json';
+import dotenv from 'dotenv';
 
 const DATABASE_NAME = 'appdb';
+dotenv.config();
 
 const wrapper = {
   getMongoUri(
@@ -18,21 +20,21 @@ const wrapper = {
     if (mode === 'remote') {
       // mlab mongo database URI
       // default mlab database
-      let mongodbHost = config.host;
-      let mongodbPort = config.port;
-      let mongodbUser = config.username;
-      let mongodbPass = config.password;
+      // let mongodbHost = config.host;
+      // let mongodbPort = config.port;
+      // let mongodbUser = config.username;
+      // let mongodbPass = config.password;
 
       // custom mlab database URI
-      if (arguments.length === 5) {
-        mongodbHost = host;
-        mongodbPort = port;
-        mongodbUser = user;
-        mongodbPass = pass;
-      }
+      // if (arguments.length === 5) {
+      //   mongodbHost = host;
+      //   mongodbPort = port;
+      //   mongodbUser = user;
+      //   mongodbPass = pass;
+      // }
 
-      const mongoURI = `mongodb://${mongodbUser}:${mongodbPass}@${mongodbHost}:${mongodbPort}/${DATABASE_NAME}`;
-
+      // const mongoURI = `mongodb://${mongodbUser}:${mongodbPass}@${mongodbHost}:${mongodbPort}/${DATABASE_NAME}`;
+      const mongoURI = process.env.DATABASE_URL;
       return mongoURI;
     }
   },
--- app/server.ts
+++ app/server.ts
@@ -9,14 +9,19 @@ import app from './app';
 
 const certPath = 'cert';
 
-const DEFAULT_URI: string | undefined = dbconfig.getMongoUri(); //  get the URI from config file
+const DEFAULT_URI: string | undefined = dbconfig.getMongoUri(); //  get the URI from config file
 
 const DEFAULT_PORT: number = 3000;
 
+try {
 mongoose.connect(
   DEFAULT_URI,
   { useNewUrlParser: true },
-);
+).catch(err => console.log(err))
+}
+catch(err) {
+  console.log(err)
+}
 
 const server = app.listen(process.env.PORT || DEFAULT_PORT, () => {
   const port = server.address().port;
