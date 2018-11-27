b44d5c2f90611dff2092ee83967564a5969a8d64
ayshiff <remi.d45@gmail.com>
2018-11-27 18:47:53 +0100

    improve: add cloudinary support2

--- app/app.ts
+++ app/app.ts
@@ -1,19 +1,20 @@
-import express, {Router, Request, Response} from 'express'; //  call express
-import bodyParser from 'body-parser';
+import express, { Router, Request, Response } from "express"; //  call express
+import bodyParser from "body-parser";
 
-import Post from './routes/post';
-import Get from './routes/get';
-import Auth from './routes/auth';
-import enforce from 'express-sslify';
+import Post from "./routes/post";
+import Get from "./routes/get";
+import Auth from "./routes/auth";
+import enforce from "express-sslify";
 
 const app: express.Application = express(); // use express on our app
 
-if (process.env.NODE_ENV === 'production' ) app.use(enforce.HTTPS({trustProtoHeader: true})); // Redirect http => https
+if (process.env.NODE_ENV === "production")
+  app.use(enforce.HTTPS({ trustProtoHeader: true })); // Redirect http => https
 
 const listOfRoutes = (router: Router) => {
-    Post(router);
-    Get(router);
-    Auth(router);
+  Post(router);
+  Get(router);
+  Auth(router);
 };
 
 const router: Router = express.Router(); // get an instance of the express Router
@@ -21,18 +22,18 @@ const router: Router = express.Router(); // get an instance of the express Route
 listOfRoutes(router);
 
 router.use((req: Request, res: Response, next) => {
-    console.log(req.connection.remoteAddress);
-    next();
+  console.log(req.connection.remoteAddress);
+  next();
 });
 
-router.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'It works !' });
+router.get("/", (req: Request, res: Response) => {
+  res.json({ message: "It works !" });
 });
 
 // configure app to use bodyParser() => get data from http request (POST)
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
 
-app.use('/api', router); // define the default route
+app.use("/api", router); // define the default route
 
 export default app;
