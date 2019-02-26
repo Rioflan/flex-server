5e1d54e8465597505079ef7686810c778e903d25
GarySchwab <gary.schwab@outlook.fr>
2019-02-26 11:15:52 +0100

    Changed syntax for more clarity

--- app/server.ts
+++ app/server.ts
@@ -42,11 +42,15 @@ websocket.on('connect', (socket) => {
 });
 
 placesCollection.watch({ fullDocument: 'updateLookup' }).on('change', (changes) => {
-    const place = changes.fullDocument;
-    if (place && place.using === false) {
-        if (websocket.sockets.adapter.rooms[place.id])
-            websocket.in(place.id).emit('leavePlace');
-        else
-            pool = [...pool, place.id];
+    if (changes.fullDocument) { // fullDocument is undefined if the operation is not an update (e.g. insert or remove)
+        const place = changes.fullDocument;
+        if (place.using === false) {
+            // if the user is disconnected, the room doesn't exist
+            const userConnected = websocket.sockets.adapter.rooms[place.id];
+            if (userConnected)
+                websocket.in(place.id).emit('leavePlace');
+            else
+                pool.push(place.id);
+        }
     }
 });
