2410e4c03d77e16c68cc1b6b2efbc476b57fb7a8
Gary Schwab <gary.schwab@outlook.fr>
2019-04-24 11:48:39 +0200

    Update users directly during cron task

--- bin/remove.sh
+++ bin/remove.sh
@@ -1,1 +1,2 @@
-db.places.update({using: true}, {$set: {using: false, id_user: ""}}, {multi: true})
+db.places.update({ using: true }, { $set: { using: false, id_user: "" } }, { multi: true })
+db.users.update({}, { $set: { id_place: "" } }, { multi: true })
