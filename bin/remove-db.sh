de75f4884f0ada188c830ce8ff15d8207ef109cd
Gary Schwab <gary.schwab@outlook.fr>
2019-04-25 13:51:12 +0200

    Remove cron task files because we now use the API

--- bin/remove-db.sh
+++
@@ -1,1 +0,0 @@
-mongo "mongodb+srv://cluster0-rjkbw.mongodb.net/flex" --username GarySchwab --password $DB_PASSWORD_TMP < ./bin/remove.sh
--- bin/remove.sh
+++
@@ -1,2 +0,0 @@
-db.places.update({ using: true }, { $set: { using: false, id_user: "" } }, { multi: true })
-db.users.update({}, { $set: { id_place: "" } }, { multi: true })
