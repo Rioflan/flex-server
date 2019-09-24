c94467a08ffcf047c7132dd1437d8a0fb75ba232
Jan ATAC <can.atac@gmail.com>
2019-09-24 15:34:16 +0200

    add azure deployment npm start 2

--- deploy.sh
+++ deploy.sh
@@ -114,6 +114,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install --production"
   eval $NPM_CMD install --production
+  npm run build
   npm start
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
