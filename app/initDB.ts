68bffc8ca2f8c1bc25dd9ae375d25e2559b9c097
Jan ATAC <can.atac@gmail.com>
2019-10-11 17:24:44 +0200

    change db init

--- app/initDB.ts
+++ app/initDB.ts
@@ -1,20 +1,8 @@
-import mongoose = require("mongoose")
-import dbconfig from './database/mongoDB';
+import mongoose = require("mongoose");
 import Places from "./models/place";
-import apiUser, {ApiSchema} from './models/apikey';
-import credentials from "./credentials.json";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import credentials from "./credentials_LOCAL.json";
+import { domainToASCII } from "url";
 
-
-/**
- * API TOKEN
- */
-
-const usermail = process.env.USERMAIL || credentials.usermail;
-const username = process.env.USERNAME || credentials.username;
-const password = process.env.PASSWORD || credentials.password;
-const hashedPassword = bcrypt.hashSync(password, 8);
 process.stdout.write("IMPORTING....\n");
 /**
  * PLACES
@@ -58,108 +46,103 @@ const floor4 = {
     }
 };
 
-const floorX = {
-    "number": 0,
-    "V": {
-        "SEINE": 23,
-        "COUR": 13
-    },
-    "B": {
-        "SEINE": 23,
-        "COUR": 13
-    },
-    "R": {
-        "SEINE": 23,
-        "COUR": 13
+const floor10 = {
+    "number": 10,
+    "A": {
+        "SEINE": 16,
+        "COUR": 38
     }
 };
-
+const floor11 = {
+    "number": 11,
+    "A": {
+        "SEINE": 24,
+        "COUR": 30
+    }
+};
 const floors = [floor3, floor4];
-const floorsX = [floorX];
+const floorsX = [floor10, floor11];
 const zones = ["V", "B", "R"];
+const zonesLR = ["A"];
+
 const sides = ["RER", "MILIEU", "BOIS"];
 const sides2 = ["SEINE", "COUR"];
 const nbFloors = floors.length;
+const nbFloorsLR = floorsX.length;
+
 const nbZones = zones.length;
+const nbZonesLR = zonesLR.length;
 const nbSides = sides.length;
+const nbSidesLR = sides2.length;
 
 
 (async () => {
     try{
-        mongoose.connect(dbconfig.getMongoUri(), { useNewUrlParser: true });
-        mongoose.set('useFindAndModify', false);
-        // RAJOUT DU USER API
-        
-        process.stdout.write("ADDING API USER\n");
-        await apiUser.create(
-            {
-                name: username,
-                email: usermail,
-                api_key: hashedPassword,
-                creation: Date.now(),
-            },
-            (err: Error, user) => {
-                if (err) return process.stdout.write("ERROR ? "+err+"\n");
-            },
-        );
-        await apiUser.findOne({email: usermail}, (err: Error, user: ApiSchema) => {
-            if (err) return process.stdout.write('Error on the server.\n');
-            if (!user) return process.stdout.write('No user found.\n');
+        // Read the certificate authority
 
-            const token = jwt.sign({id: user._id}, process.env.API_SECRET || credentials.api_secret);
-
-            process.stdout.write("API USER INSERTED :" +username+", "+usermail+"\n");
-            process.stdout.write("API TOKEN (keep it for API Authentication):" +token+"\n");
-        });    
-    
-    // LA RAPEE
-        let totalX = 0;
-            for (let indexFloor = 0; indexFloor < floorsX.length; indexFloor++) {
-                for (let indexZone = 0; indexZone < nbZones; indexZone++) {
-                    for (let indexSide = 0; indexSide < sides2.length; indexSide++) {
-                        let number = floorsX[indexFloor].number;
-                        let zone = zones[indexZone];
-                        let side = sides2[indexSide];
-                        let nbPlaces = floorsX[indexFloor][zone][side];
-                        for (let numPlace = 1; numPlace <= nbPlaces; numPlace++) {
-                            let id = `RA-${number}-${zone}-${side}${("0" + numPlace).slice(-2)}`;
-                            process.stdout.write("\r                              \r"); // clear current line
-                            process.stdout.write("Inserting " + id + "\n");
-                            await Places.findOneAndUpdate({id: id}, {}, {upsert: true, setDefaultsOnInsert: true});
-                            totalX++;
+        mongoose.connect(credentials.cosmosdb_url, {
+            auth: {
+                user: credentials.cosmosdb_user,
+                password: credentials.cosmosdb_pwd
+              },
+            useNewUrlParser: true,
+          })
+          .then(async () => {
+              console.log('Connection to CosmosDB successful');
+                  
+                // LA RAPEE
+                let totalX = 0;
+                for (let indexFloor = 0; indexFloor < floorsX.length; indexFloor++) {
+                    for (let indexZone = 0; indexZone < nbZonesLR; indexZone++) {
+                        for (let indexSide = 0; indexSide < sides2.length; indexSide++) {
+                            let number = floorsX[indexFloor].number;
+                            let zone = zonesLR[indexZone];
+                            let side = sides2[indexSide];
+                            let nbPlaces = floorsX[indexFloor][zone][side];
+                            for (let numPlace = 1; numPlace <= nbPlaces; numPlace++) {
+                                let id = `RA-${number}-${zone}-${side}${("0" + numPlace).slice(-2)}`;
+                                process.stdout.write("\r                              \r"); // clear current line
+                                process.stdout.write("Inserting " + id + "\n");
+                                await Places.findOneAndUpdate({id: id}, {}, {upsert: true, setDefaultsOnInsert: true});
+                                totalX++;
+                            }
                         }
                     }
                 }
-            }
-        
-        
-        let total = 0;
-        for (let indexFloor = 0; indexFloor < nbFloors; indexFloor++) {
-            for (let indexZone = 0; indexZone < nbZones; indexZone++) {
-                for (let indexSide = 0; indexSide < nbSides; indexSide++) {
-                    let number = floors[indexFloor].number;
-                    let zone = zones[indexZone];
-                    let side = sides[indexSide];
-                    let nbPlaces = floors[indexFloor][zone][side];
-                    for (let numPlace = 1; numPlace <= nbPlaces; numPlace++) {
-                        let id = `JO-${number}-${zone}-${side}${("0" + numPlace).slice(-2)}`;
-                        process.stdout.write("\r                              \r"); // clear current line
-                        process.stdout.write("Inserting " + id + "\n");
-                        await Places.findOneAndUpdate({id: id}, {}, {upsert: true, setDefaultsOnInsert: true});
-                        total++;
+
+
+            let total = 0;
+                for (let indexFloor = 0; indexFloor < nbFloors; indexFloor++) {
+                    for (let indexZone = 0; indexZone < nbZones; indexZone++) {
+                        for (let indexSide = 0; indexSide < nbSides; indexSide++) {
+                            let number = floors[indexFloor].number;
+                            let zone = zones[indexZone];
+                            let side = sides[indexSide];
+                            let nbPlaces = floors[indexFloor][zone][side];
+                            for (let numPlace = 1; numPlace <= nbPlaces; numPlace++) {
+                                let id = `JO-${number}-${zone}-${side}${("0" + numPlace).slice(-2)}`;
+                                process.stdout.write("\r                              \r"); // clear current line
+                                process.stdout.write("Inserting " + id + "\n");
+                                await Places.findOneAndUpdate({id: id}, {}, {upsert: true, setDefaultsOnInsert: true});
+                                total++;
+                            }
+                        }
                     }
                 }
-            }
-        }
-        console.log("\nTotal LA RAPEE :", totalX, "inserted");
-        console.log("\nTotal JOINVILLE :", total, "inserted");
+                console.log("\nTotal LA RAPEE :", totalX, "inserted");
+                console.log("\nTotal JOINVILLE :", total, "inserted");
 
-        console.log("\nTotal GLOBAL :", total+totalX, "inserted");
-        await mongoose.disconnect();
-        console.log("FINISHED !");
+                console.log("\nTotal GLOBAL :", total+totalX, "inserted");
+                await mongoose.disconnect();
+                console.log("FINISHED !");
+            })
+          .catch((err) => console.error(err));
+
+
     }
     catch(err){
         console.log("ERROR : "+err);
 
     }  
 })();
+
